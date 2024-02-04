f581ea78fcaf1aabdfa68338c36d350e016ed493
yamachan0625 <napoli.080625@gmail.com>
2024-02-05 03:59:30 +0900

    add vpc & subnet

---
+++ main.tf
@@ -0,0 +1,32 @@
+# # -----------------
+# # Terraform configurrations
+# # -----------------
+# terraform {
+#   required_version = "1.7.2"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~>3.0"
+#     }
+#   }
+# }
+
+# # -----------------
+# # Provider
+# # -----------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# # -----------------
+# # Variables
+# # -----------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
