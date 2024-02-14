4b1a45487d4dbee9e9f1a650eec9b42e9203b484
yamachan0625 <napoli.080625@gmail.com>
2024-02-15 02:02:06 +0900

    ssm

--- appserver.tf
+++ appserver.tf
@@ -18,6 +18,7 @@ resource "aws_key_pair" "keypair" {
 resource "aws_instance" "app_server" {
   ami                         = data.aws_ami.app.id
   instance_type               = "t2.micro"
+  iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
   key_name                    = aws_key_pair.keypair.key_name
   subnet_id                   = aws_subnet.public_subnet_1a.id
   associate_public_ip_address = true
@@ -29,4 +30,37 @@ resource "aws_instance" "app_server" {
     Env     = var.environment
     Type    = "app"
   }
+}
+
+# # -----------------
+# # SSM Parameter Store
+# # -----------
+resource "aws_ssm_parameter" "host" {
+  name  = "${var.project}/${var.environment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "${var.project}/${var.environment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "${var.project}/${var.environment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.db_name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "${var.project}/${var.environment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "${var.project}/${var.environment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
 }
